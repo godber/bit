af1784ba415793aa774d491b23ff184c0c5962be
Austin Godber <godber@uberhip.com>
2025-11-02 12:13:19 -0700

    change scaling

--- src/hooks/useAnimationControls.js
+++ src/hooks/useAnimationControls.js
@@ -18,7 +18,7 @@ export function useAnimationControls() {
     noPulseAmp: { value: 0.05, min: 0, max: 0.2, step: 0.01 },
     outerPulseFreq: { value: 2.1, min: 0, max: 10, step: 0.1 },
     innerPulseFreq: { value: 2.6, min: 0, max: 10, step: 0.1 },
-    innerScaleRatio: { value: 0.75, min: 0.5, max: 1.0, step: 0.01 },
+    innerScaleRatio: { value: 0.92, min: 0.5, max: 1.0, step: 0.01 },
     innerPhaseOffset: { value: Math.PI * 0.35, min: 0, max: Math.PI * 2, step: 0.01 },
     innerAmpMultiplier: { value: 1.2, min: 0.5, max: 3, step: 0.1 },
     shimmerAmplitude: { value: 0.1, min: 0, max: 0.5, step: 0.01 },
